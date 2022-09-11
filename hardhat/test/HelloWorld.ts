dd6a433a5936ea1f7ec257ad7e979ad2f817840b
Luis Martinez <luismartinezwebdev@gmail.com>
2022-09-11 11:57:49 +0200

    feat: use fixture

--- hardhat/test/HelloWorld.ts
+++ hardhat/test/HelloWorld.ts
@@ -1,16 +1,24 @@
 import { expect } from "chai";
 import hre from "hardhat";
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 
 describe("HelloWorld", function () {
-  it("Should be able to get meaning", async function () {
+  async function deployHelloWorld () {
     const HelloWorld = await hre.ethers.getContractFactory("HelloWorld");
     const helloWorld = await HelloWorld.deploy();
+
+    return {
+      helloWorld
+    }
+  }
+
+  it("Should be able to get meaning", async function () {
+    const {helloWorld} = await loadFixture(deployHelloWorld)
 
     expect(await helloWorld.getMeaning()).to.equal(42);
   });
   it("Should be able to set meaning", async function () {
-    const HelloWorld = await hre.ethers.getContractFactory("HelloWorld");
-    const helloWorld = await HelloWorld.deploy();
+    const {helloWorld} = await loadFixture(deployHelloWorld)
 
     await helloWorld.setMeaning(43);
     expect(await helloWorld.getMeaning()).to.equal(43);
