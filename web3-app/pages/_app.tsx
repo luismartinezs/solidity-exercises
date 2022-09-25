e700f1bacd2b54d89c61676a097979faff7182dc
Luis Martinez <luismartinezwebdev@gmail.com>
2022-09-25 19:16:10 +0200

    feat: add theme and header

--- web3-app/pages/_app.tsx
+++ web3-app/pages/_app.tsx
@@ -1,8 +1,39 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Link from "next/link";
+
+const linkList = [
+  {
+    href: "/",
+    label: "Home",
+  },
+  {
+    href: "/get-set",
+    label: "Get Set",
+  },
+];
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <header>
+        <nav>
+          <ul className="flex space-x-4">
+            {linkList.map((link) => (
+              <li key={link.href}>
+                <Link href={link.href}>
+                  <a className="text-blue-500 hover:text-blue-600">
+                    {link.label}
+                  </a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- web3-app/tailwind.config.js
+++ web3-app/tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.blue,
+        secondary: colors.indigo,
+        gray: colors.zinc
+      },
+    },
   },
   plugins: [],
 }
